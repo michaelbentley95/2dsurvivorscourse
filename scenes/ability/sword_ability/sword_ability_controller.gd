9aa18824abcb27d1b49f933cefe395533956e026
michaelbentley95 <44556637+michaelbentley95@users.noreply.github.com>
2025-06-29 16:04:21 -0500

    Abstract hitboxes and hurtboxes

--- scenes/ability/sword_ability/sword_ability_controller.gd
+++ scenes/ability/sword_ability/sword_ability_controller.gd
@@ -3,6 +3,7 @@ extends Node
 const MAX_RANGE = pow(150,2) #square the max range since it's a distance
 
 @export var sword_ability: PackedScene
+var damage = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,8 +28,9 @@ func on_timer_timeout():
 		return a_distance < b_distance
 	)
 	
-	var sword_instance = sword_ability.instantiate() as Node2D
+	var sword_instance = sword_ability.instantiate() as SwordAbility
 	player.get_parent().add_child(sword_instance)
+	sword_instance.hitbox_component.damage = damage
 	sword_instance.global_position = enemies[0].global_position + (Vector2.RIGHT.rotated(randf_range(0,TAU)) * 4)
 	
 	#get enemy direction and rotate to that direction
