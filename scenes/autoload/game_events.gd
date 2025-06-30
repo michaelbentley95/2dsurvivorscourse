19f2a10e8f6dfd83b08728f3b2825741f0cb3cf2
Mike Bentley <michaelbentley95@gmail.com>
2025-06-29 22:06:28 -0500

    Make the ability upgrades work

--- scenes/autoload/game_events.gd
+++ scenes/autoload/game_events.gd
@@ -1,6 +1,10 @@
 extends Node
 
 signal experience_orb_collected(collected_experience: float)
+signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary)
 
 func emit_experience_orb_collected(number: float):
 	experience_orb_collected.emit(number)
+
+func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	ability_upgrade_added.emit(upgrade, current_upgrades)
