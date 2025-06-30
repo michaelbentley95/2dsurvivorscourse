31f28b81428bd47ee57d49f1f0e1bb0c447f01e4
michaelbentley95 <44556637+michaelbentley95@users.noreply.github.com>
2025-06-29 19:49:16 -0500

    Add ability to select an upgrade and apply to UpgradeManager

--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 class_name UpgradeScreen
 
+signal upgrade_selected(upgrade: AbilityUpgrade)
+
 @export var upgrade_card_scene: PackedScene
 @onready var card_container: HBoxContainer = $%CardContainer
 
@@ -12,3 +14,9 @@ func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
 		var card_instance = upgrade_card_scene.instantiate() as AbilityUpgradeCard
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	upgrade_selected.emit(upgrade)
+	get_tree().paused = false
+	queue_free()
